7ba36ab898a16f32c42102cedcafaecbc69a5ae2
AndrejTsanda <tsanda.andrej@gmail.com>
2020-06-26 22:17:11 +0300

    Improved configuration reading.

--- src/main/java/com/tsanda/sftpDownloader/SFTPDownloader.java
+++ src/main/java/com/tsanda/sftpDownloader/SFTPDownloader.java
@@ -6,13 +6,9 @@ import com.jcraft.jsch.ChannelSftp.LsEntry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Vector;
+import java.util.*;
 
 public class SFTPDownloader {
 
@@ -23,41 +19,37 @@ public class SFTPDownloader {
     String addressConfigFile = args[0];
 
     System.out.println("--- SFTP DOWNLOADER ---");
-
-    File file = new File(addressConfigFile);
-    Map<String, String> mapConfig = new HashMap<String, String>();
+    Properties properties = new Properties();
     try {
-      Scanner scanner = new Scanner(file);
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        String[] dataConfig = line.split("=");
-        mapConfig.put(dataConfig[0], dataConfig[1]);
-      }
+      InputStream inputStream = new FileInputStream(addressConfigFile);
+      properties.load(inputStream);
     } catch (FileNotFoundException e) {
-      System.out.println("Config File not Found");
+      logger.warn(e);
+    } catch (IOException e) {
+      logger.warn(e);
     }
 
     SFTPClient sftpClient =
         new SFTPClient(
-            mapConfig.get("sftp_host"),
-            mapConfig.get("sftp_user"),
-            mapConfig.get("sftp_password"),
-            Integer.parseInt(mapConfig.get("sftp_port")));
+            properties.getProperty("sftp_host"),
+            properties.getProperty("sftp_user"),
+            properties.getProperty("sftp_password"),
+            Integer.parseInt(properties.getProperty("sftp_port")));
     sftpClient.connect();
     MysqlClient mysqlClient =
         new MysqlClient(
-            mapConfig.get("sql_database") + "?serverTimezone=UTC", // serverTimezone=UTC",
-            mapConfig.get("sql_user"),
-            mapConfig.get("sql_password"));
+            properties.getProperty("sql_database"),
+            properties.getProperty("sql_user"),
+            properties.getProperty("sql_password"));
     mysqlClient.connect();
 
     logger.debug("Downloading...");
-    Vector listFiles = sftpClient.getListFiles(mapConfig.get("sftp_remote_dir"));
+    Vector listFiles = sftpClient.getListFiles(properties.getProperty("sftp_remote_dir"));
     for (Object files : listFiles) {
       LsEntry lsEntry = (LsEntry) files;
       sftpClient.download(
-          mapConfig.get("sftp_remote_dir") + lsEntry.getFilename(),
-          mapConfig.get("local_dir") + lsEntry.getFilename());
+          properties.getProperty("sftp_remote_dir") + lsEntry.getFilename(),
+          properties.getProperty("local_dir") + lsEntry.getFilename());
       mysqlClient.save(lsEntry.getFilename(), new Timestamp(System.currentTimeMillis()));
     }
     logger.debug("Download is done.");
